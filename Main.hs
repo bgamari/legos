ce903a5bac720a34658cf235a38698deea90931a
Ben Gamari <bgamari.foss@gmail.com>
2012-10-10 19:24:46 -0400

    Add command line arguments

--- Main.hs
+++ Main.hs
@@ -23,6 +23,8 @@ import           Track
 
 data TrackOpts = TrackOpts { file        :: FilePath
                            , maxDist     :: Dist
+                           , maxTau      :: Int
+                           , dt0         :: Int
                            }
                deriving (Show, Eq)
                
@@ -34,6 +36,16 @@ trackOpts = TrackOpts
                     <> value 20
                     <> short 'd'
                     <> help "Maximum distance"
+                     )
+    <*> option       ( long "max-tau"
+                    <> short 'T'
+                    <> value 3000
+                    <> help "Maximum tau in frames"
+                     )
+    <*> option       ( long "dt0"
+                    <> short 't'
+                    <> value 10
+                    <> help "Step of dt0"
                      )
 
 tabSep = defaultEncodeOptions { encDelimiter = 9 }
@@ -55,16 +67,16 @@ main = do
             $ encodeWith tabSep $ V.map (\(t,P (x,y))->(t,x,y)) v
         BS.writeFile (printf "track-%03d.displ" k)
             $ encodeWith tabSep $ V.fromList
-            $ map (\tau-> (tau, mean $ meanSqDispl v tau)) 
-            $ map round $ logspace 1 1000 100
+            $ map (\tau -> (tau, mean $ meanSqDispl (dt0 args) v tau)) 
+            $ map round $ logspace 1 (realToFrac $ maxTau args) 100
     
 logspace :: RealFloat a => a -> a -> Int -> [a]
 logspace a b n = [ exp $ log a + realToFrac i * d | i <- [0..n-1]]
     where d = (log b - log a) / realToFrac n
-    
-meanSqDispl :: V.Vector DataPoint -> Time -> V.Vector Dist
-meanSqDispl points tau =
-    V.fromList $ mapMaybe f $ tailsV 10 points
+
+meanSqDispl :: Time -> V.Vector DataPoint -> Time -> V.Vector Dist
+meanSqDispl dt0 points tau =
+    V.fromList $ mapMaybe f $ tailsV dt0 points
     where f :: V.Vector DataPoint -> Maybe Dist
           f v | V.null v'  = Nothing
               | otherwise  = let (_,x)  = V.head v
@@ -75,5 +87,4 @@ meanSqDispl points tau =
 tailsV :: Int -> V.Vector a -> [V.Vector a]            
 tailsV n v | V.null v = []
            | otherwise = v : tailsV n (V.drop n v)
-   
- 
+
